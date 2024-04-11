e6d9cbeef81824f974a888e4ae1a978aa00e1af9
Michael Mgebrishvili <mmd920802@gmail.com>
2024-04-11 19:17:32 +0300

    localstorage

--- src/components/WeatherService/WeatherService.tsx
+++ src/components/WeatherService/WeatherService.tsx
@@ -4,10 +4,12 @@ import { cnWeatherService } from './WeatherService.classname';
 
 import './WeatherService.css';
 
+const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
+
 const WeatherService = () => {
-    
+
     return (
-        <div className={cnWeatherService('Widget')}>
+        <div className={cnWeatherService('')}>
         </div>
     );
 }
